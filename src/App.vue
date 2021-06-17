687325fa8682ebd6e62748c56ea334a198d5efb5
Santtu Nykänen <santtu.nykanen@solidabis.com>
2021-06-18 02:38:37 +0300

    convert to vue app

---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <header id="nav">
+    <aside aria-label="Linkit">
+      <a href="https://www.linkedin.com/in/santtu-nykanen/">LinkedIn</a>
+      <span> | </span>
+      <a href="https://github.com/cyanidesayonara">GitHub</a>
+    </aside>
+    <router-link to="/">FI</router-link>
+    <span> | </span>
+    <router-link to="/en">EN</router-link>
+  </header>
+  <router-view />
+</template>
+
+<style lang="scss">
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: auto;
+}
+
+a {
+  font-weight: bold;
+  color: #2cb3ac;
+
+  &.router-link-exact-active {
+    color: #2c3e50;
+  }
+}
+html {
+  box-sizing: border-box;
+}
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+body {
+  font-family: "Titillium Web", sans-serif;
+  margin: 0;
+}
+header,
+article,
+footer {
+  padding: 20px;
+}
+aside {
+  float: right;
+}
+img {
+  border-radius: 0.2rem;
+  box-shadow: rgba(0, 0, 0, 0.5) 0 0 20px;
+  width: 40%;
+  margin: 5px;
+  float: right;
+}
+hr {
+  margin: 0 -20px;
+  border: 0;
+  height: 1px;
+  background-image: linear-gradient(
+    to right,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.1) 10%,
+    rgba(0, 0, 0, 0.2) 50%,
+    rgba(0, 0, 0, 0.1) 90%,
+    rgba(0, 0, 0, 0) 100%
+  );
+}
+a {
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/views/Portfolio.vue
@@ -0,0 +1,245 @@
+<template>
+  <section>
+    <article>
+      <img src="../assets/photo.png" alt="Kuva minusta" />
+      <h1>Moi ツ</h1>
+      <p>
+        Olen Santtu Nykänen, kolmikymppinen fullstack-kehittäjä Helsingistä.
+      </p>
+      <p>
+        Vaihdoin nelisen vuotta sitten alaa ja olen työskennellyt kehittäjänä
+        nyt reilut pari vuotta. Opiskelen työn ohessa softakehitystä Haaga-Helia
+        AMK:ssa aikuislinjalla.
+      </p>
+      <p>
+        Töissä olen koodaillut enimmäkseen Pythonia, Javaa ja JavaScriptia, ei
+        missään erityisessä järjestyksessä. Viime aikoina olen opiskellut myös
+        esim. Gota, Flutteria ja Kotlinia.
+      </p>
+      <p>
+        Tämä portfolio sisältää lyhyesti tietoa omista projekteistani sekä
+        linkkejä nettiprofiileihin, kuten
+        <a href="https://www.linkedin.com/in/santtu-nykanen/">LinkedIn</a>
+        ja
+        <a href="https://github.com/cyanidesayonara">GitHub</a>, joissa on
+        todennäköisesti aina ajankohtaisempaa ja tarkempaa tietoa. Kiitos
+        käynnistä!
+      </p>
+    </article>
+    <hr />
+    <article>
+      <h2>Työkokemus</h2>
+      <ul>
+        <li>
+          <aside aria-label="Linkit">
+            <a
+              rel="noreferrer"
+              href="https://www.linkedin.com/company/solidabis/"
+              >LinkedIn</a
+            >
+          </aside>
+          <h3>Solidabis Oy</h3>
+          <p>
+            Softakehitystä konsulttina erinäisissä projekteissa sekä backend-
+            frontend-rooleissa. Alkanut maaliskuussa 2019.
+          </p>
+          <p>
+            Avainsanat: <em>Java, JavaScript, Python, SQL, React, Spring</em>
+          </p>
+        </li>
+      </ul>
+    </article>
+    <hr />
+    <article>
+      <h2>Henkilökohtaisia projekteja</h2>
+      <ul>
+        <li>
+          <aside aria-label="Linkit">
+            <a href="https://varastolista.herokuapp.com/">Heroku</a>
+            <span> | </span>
+            <a href="https://github.com/cyanidesayonara/varastolista">GitHub</a>
+          </aside>
+          <h3>Säteilytekniikan varastolista</h3>
+          <p>
+            Varastolista, jonka tein opiskelijana HUS:n säteilytekniikan
+            osastolle. Melko simppeli CRUD-appi, jolla käyttäjä voi hakea,
+            lisätä, muokata, tai poistaa varaosia. Osalistan voi myös importata
+            ja exportata excelinä. Osanumeroiden lukemisen avuksi on
+            QR-koodinlukija, joka käyttää puhelimen kameraa.
+          </p>
+          <p>
+            Avainsanat:
+            <em
+              >Python, Django, JavaScript, jQuery, PostgreSQL, Instascan,
+              Heroku</em
+            >
+          </p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a href="https://github.com/cyanidesayonara/monitorimittari"
+              >GitHub</a
+            >
+          </aside>
+          <h3>Monitorimittari</h3>
+          <p>
+            Toinen HUS:n säteilytekniikalle tehty Python-ohjelma. Tällä voidaan
+            kirjata LXscan-luminanssimittarilla mitattuja testituloksia
+            sapluunana käytettävään .xlsx-tiedostoon.
+          </p>
+          <p>Avainsanat: <em>Python, PyQt5, PyInstaller</em></p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a href="https://github.com/cyanidesayonara/dopepod">GitHub</a>
+          </aside>
+          <h3>dopepod</h3>
+          <p>
+            Web app, joka noutaa podcasteja iTunesin API:n avulla (ja kaapii
+            lisätietoa muista lähteistä). Käyttäjä voi etsiä ja soittaa satoja
+            tuhansia ilmaisia podcasteja, sekä rekisteröityneenä myös tehdä
+            tilauksia ja muodostaa soittolistan.
+          </p>
+          <p>
+            Avainsanat:
+            <em>
+              Python, Django, JavaScript, jQuery, Bootstrap, PostgreSQL, Scrapy,
+              DigitalOcean, Haystack, Elasticsearch
+            </em>
+          </p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a href="https://dop3pod.herokuapp.com/">Heroku</a>
+            <span> | </span>
+            <a href="https://github.com/cyanidesayonara/dop3pod">GitHub</a>
+          </aside>
+          <h3>dop3pod</h3>
+          <p>
+            Versio 3.0 ylläolevasta lemmikkiprojektistani. Pyrkii toteuttamaan
+            vastaavan toiminnallisuuden modernimmilla teknologioilla ja
+            kehittyneemmällä arkkitehtuurilla. Tärkeimpänä uutuutena projektissa
+            on Go-backend ja Flutterilla kehitetty Android/iOS-app.
+          </p>
+          <p>
+            Avainsanat:
+            <em>
+              Python, Django Rest Framework, Celery, Redis, Go, Flutter, Docker,
+              PostgreSQL, Scrapy, DigitalOcean
+            </em>
+          </p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a href="https://noteowl.herokuapp.com/">Heroku</a>
+            <span> | </span>
+            <a href="https://github.com/cyanidesayonara/noteowl">GitHub</a>
+          </aside>
+          <h3>NoteOwl</h3>
+          <p>
+            Web appi, jolla voi kirjata muistiinpanoja sekä assosioida niitä
+            kalenteripäivämäärien mukaan. Frontendinä käytössä on React ja
+            backendinä Node. Tietokanta on MongoDB.
+          </p>
+          <p>Avainsanat: <em>JavaScript, Node, React, MongoDB, Heroku</em></p>
+        </li>
+      </ul>
+    </article>
+    <hr />
+    <article>
+      <h2>Koulutus & muita kursseja</h2>
+      <ul>
+        <li>
+          <aside aria-label="Linkit">
+            <a
+              rel="noreferrer"
+              href="https://www.haaga-helia.fi/fi/tietojenkasittelyn-koulutus-tradenomi-amk"
+              >Linkki</a
+            >
+          </aside>
+          <h3>Tietojenkäsittelyn koulutus, tradenomi (AMK)</h3>
+          <p>
+            Tradenomin tutkinto Haaga-Helia AMK:ssa, suuntautumisalana
+            ohjelmistokehitys. Aloitettu 2018, kesken.
+          </p>
+          <p>
+            Keywords:
+            <em>Java, JavaScript, Python, SQL, HTML, CSS, React, Spring</em>
+          </p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a rel="noreferrer" href="https://fullstackopen.com/">Linkki</a>
+            <span> | </span>
+            <a href="https://github.com/cyanidesayonara/fullstack-mooc-2021"
+              >GitHub</a
+            >
+          </aside>
+          <h3>Fullstack Open 2021</h3>
+          <p>
+            Helsingin yliopiston avoin fullstack-ohjelmoinnin verkkokurssi.
+            Suoritettu 2021.
+          </p>
+          <p>
+            Avainsanat:
+            <em>React, Redux, Node, MongoDB, GraphQL, TypeScript</em>
+          </p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a rel="noreferrer" href="https://www.elementsofai.com/fi"
+              >Linkki</a
+            >
+          </aside>
+          <h3>Elements of AI</h3>
+          <p>
+            Helsingin yliopiston tarjoama avoin verkkokurssi tekoälyn
+            perusteista. Suoritettu 2019.
+          </p>
+          <p>Avainsanat: <em>tekoäly, koneoppiminen</em></p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a rel="noreferrer" href="https://2017-ohjelmointi.github.io/"
+              >Linkki</a
+            >
+            <span> | </span>
+            <a href="https://github.com/cyanidesayonara/java-mooc">GitHub</a>
+          </aside>
+          <h3>Ohjelmoinnin perusteet ja jatkokurssi 2017</h3>
+          <p>
+            Helsingin yliopiston kaksiosainen avoin Java-ohjelmoinnin
+            verkkokurssi. Suoritettu 2017.
+          </p>
+          <p>Avainsanat: <em>Java</em></p>
+        </li>
+        <li>
+          <aside aria-label="Linkit">
+            <a
+              rel="noreferrer"
+              href="https://www.edx.org/course/introduction-computer-science-harvardx-cs50x"
+              >Linkki</a
+            >
+            <span> | </span>
+            <a href="https://github.com/cyanidesayonara/cs50-mooc">GitHub</a>
+          </aside>
+          <h3>CS50X</h3>
+          <p>
+            Harvardin avoin tietojenkäsittelytieteen verkkokurssi. Tämä kurssi
+            sai alunperin innostumaan koodauksen opiskelusta. Suoritettu 2017.
+          </p>
+          <p>Avainsanat: <em>C, Python, JavaScript, SQL, HTML, CSS</em></p>
+        </li>
+      </ul>
+    </article>
+    <hr />
+    <footer>
+      <aside aria-label="Linkit">
+        <a href="https://www.linkedin.com/in/santtu-nykanen/">LinkedIn</a>
+        <span> | </span>
+        <a href="https://github.com/cyanidesayonara">GitHub</a>
+      </aside>
+      <span>Päivitetty 2021-06-18</span>
+    </footer>
+  </section>
+</template>
